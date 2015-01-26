374c74034cf6808d12b268fe75a6bacf3969bb71
TheGallig <andy.gallagher@bhasoftware.com>
2015-01-26 09:42:56 +0000

    Upload

---
+++ js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿$(window).on('resize', function () {
+    $('.halfwidth1').width($(window).width() / 4);
+}).resize();
+
+$(window).on('resize', function () {
+    $('.halfwidth2').width($(window).width() / 4);
+}).resize();
+
+$(window).on('resize', function () {
+    $('.halfwidth3').width($(window).width() / 4);
+}).resize();
+
+$(window).on('resize', function () {
+    $('a').width($(window).width() / 4);
+}).resize();
